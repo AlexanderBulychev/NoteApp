f2aff3900fd379f0092ccee426be9e31e41ca15e
Alexander Bulychev <asbul@yandex.ru>
2022-03-28 11:10:02 +0300

    First stage

---
+++ NoteApp/Models/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  NoteApp
+//
+//  Created by asbul on 25.03.2022.
+//
+
+import Foundation
+
+struct Note {
+    var header: String
+    var body: String
+}
---
+++ NoteApp/Services/StorageManager.swift
@@ -0,0 +1,17 @@
+//
+//  StorageManager.swift
+//  NoteApp
+//
+//  Created by asbul on 28.03.2022.
+//
+
+import Foundation
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    private let userDefaults = UserDefaults()
+    
+    private init() {}
+    
+}
